cef695c1491749461c17d92e10a6dc5be5aec74a
Shimesh <yaron.orb@gmail.com>
2021-10-13 14:13:57 +0300

    PlaceMark func

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const X_CLASS = "x";
+const Circle_CLASS = "circle";
 const cellElements = document.querySelectorAll("[data-cell]");
+let circleTurn;
 
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
 });
 
 function handleClick(e) {
-  console.log("clicked");
+  const cell = e.target;
+  const currentClass = circleTurn ? Circle_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  //Check For Win
+  //Check For Draw
+  //Switch Turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
 }
